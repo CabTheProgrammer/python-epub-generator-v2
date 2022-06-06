4bdc485aeeff89b636a5592f5cc168519cd2a799
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2022-06-05 21:58:32 -0500

    Created main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+# TODO: Get contents of https://practicalguidetoevil.wordpress.com/table-of-contents/
+# Parse to get links of each chapter per book, maybe use a dictionary or 2d array?
+# download each book and compile to epub
