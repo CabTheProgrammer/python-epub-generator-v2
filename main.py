8bcce8a49970dcf2f3af8e326fc741e9089d6ac9
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2022-06-10 01:32:36 -0500

    Can now generate list of book titles

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ from bs4 import BeautifulSoup
 
 # TODO: Get contents of https://practicalguidetoevil.wordpress.com/table-of-contents/
 url =  "https://practicalguidetoevil.wordpress.com/table-of-contents/"
+title = "A Practical Guide to Evil"
 response = requests.get(url)
 # Parse to get links of each chapter per book, maybe use a dictionary or 2d array?
 soup = BeautifulSoup(response.text,"html.parser")
-linkslist=soup.find_all(class_='entry-content')
+linkslist=soup.find(class_='entry-content')
 
-for link in linkslist:
-    print(link.text)
+linkarray = []
+titlearray = []
+for books in linkslist.find_all('h2'):
+    print(books.text)
+    titlearray.append(books.text)
+
+print(titlearray)
+    
+
 
 
 # download each book and compile to epub
