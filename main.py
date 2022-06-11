51b5b2035597dd63220bdd8971212b43112d1165
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2022-06-11 12:02:06 -0500

    Now generates list of chapter links per book

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import link
 import requests
 import urllib.request
 import time
@@ -11,15 +12,43 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text,"html.parser")
 linkslist=soup.find(class_='entry-content')
 
-linkarray = []
+linkDict = {}  #Dictionary to hold books and links to each chapter
 titlearray = []
 for books in linkslist.find_all('h2'):
     print(books.text)
     titlearray.append(books.text)
 
-print(titlearray)
+
+# title array holds all the book titles
+
+# for i in range(len(linkslist.find_all('li'))):
+#     #adding to book 1
+#     barray.append(linkslist.find_all('li')[i])
+#     linkarray[titlearray[x]]=barray
+#     if linkslist.find_all('li')[i].text == "Epilogue":
+#         x+=1 
+#         barray=[]
+
+children = linkslist.findChildren('ul')
+x=0
+for i in range(len(children)-1):
+# for child in children:
+    if(i==1):
+        continue
+    print(f'I is {i}')
+    # print(f'{titlearray[x]}')
+    # print(i)
+    # print(children[i])
+    print('---------')
     
+    # if(i==6):
+    #      break
+    linkDict[titlearray[x]]=children[i]
+    x+=1
+    
+print(linkDict) 
 
 
 
+# Create Directories for each Book
 # download each book and compile to epub
